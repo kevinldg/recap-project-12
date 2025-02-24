24d527642205c0c8f8bd41008bb49cb021cea6b6
kevinldg <kevinloeding@outlook.de>
2025-02-24 16:06:40 +0100

    Extract the content of frontend.zip to /static

---
+++ src/main/java/org/example/recapproject12/RecapProject12Application.java
@@ -0,0 +1,13 @@
+package org.example.recapproject12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProject12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapProject12Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recapproject12/RecapProject12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recapproject12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProject12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
